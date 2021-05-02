f027866deaeb31b09078ad10c64c680b36b6974d
Owen Mowery <owenmowery99@gmail.com>
2021-05-02 15:31:09 -0400

    commit tree 26965604f9cc298ff67d8555b02d7c549cab8616

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validates :title_include
+
+    CLICKBAIT_PATTERNS = [ "Won't Believe", "Secret", "Top [number", "Guess"]
+
+    def title_include
+        #if the title does not contain "Won't Believe", "Secret", "Top [number]", or "Guess"
+        #the validator should add a validation
+            if title.present? && CLICKBAIT_PATTERNS.none? { |p| title.match(p) }
+                errors.add(:title, "title not clickbait-y enough")
+            end
+        end
+    end
 end
